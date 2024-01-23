7adbaae091241f65017aa35ab874920a7a0ac1b4
monari1 <francisnyasinga@gmail.com>
2024-01-24 01:22:52 +0300

    final work for the day

---
+++ src/main-page/index.js
@@ -0,0 +1,13 @@
+import './main-page.css';
+import Header from './header';
+
+function App() {
+  return (
+    <div className="container">
+     <Header subtitle = "Providing Best Housing Solutions"/>
+     
+    </div>
+  );
+}
+
+export default App;
